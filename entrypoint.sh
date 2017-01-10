d3181cc813edb2aecc18affd36986cf7aaa7c983
DragosChirila <dragos.chirila@eaudeweb.ro>
2017-01-10 14:57:57 +0200

    do not remove /var/lib/ldap/DB_CONFIG

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ mkdir -p /var/lib/ldap/eionet
 
 cp /var/lib/ldap/DB_CONFIG /var/lib/ldap/bdr
 cp /var/lib/ldap/DB_CONFIG /var/lib/ldap/eionet
-rm -f /var/lib/ldap/DB_CONFIG
 
 chown ldap.ldap /var/lib/ldap/bdr
 chown ldap.ldap /var/lib/ldap/eionet
